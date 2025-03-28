11b8fef0357a232f61b3af79539b050ec739d202
김재현 <kimjaehyun9409@gmail.com>
2025-03-29 00:57:29 +0900

    Complete pair case

--- js/test.js
+++ js/test.js
@@ -1,24 +1,68 @@
+const MAX_SEAT_LOC = 2;
 const MAX_COL = 3;
+const LIMIT_PEOPLE = 11;
+
+let idx = 0;
+let idx2 = 0;
+
+let orgPeople = 0;
+let people = 0;
+let rows = 1;
+let cols = MAX_COL;
+let seatCnt = 0;
+let blockIdx = -1;
+
+let pairSet = null;
+let isValidSeat = null;
 
 const generateChart = () => {
-  const people = parseInt(document.getElementById('people').value);
-  let rows = 0;
-  let cols = MAX_COL;
+  orgPeople = parseInt(document.getElementById('people').value);
+  people = orgPeople % 2 == 0 ? orgPeople : orgPeople + 1;
+
+  if (people >= LIMIT_PEOPLE) {
+    alert(`${LIMIT_PEOPLE-1}명까지 가능합니다`);
+    return;
+  }
+
+  if (!people || people <= 0) {
+    alert(`인원수를 입력해 주세요`);
+    return;
+  }
+
+  pairSet = Array.from({ length: people + 1 }, () => new Set());
+  isValidSeat = Array(people + 1).fill(0);
   
-  const seatCnt = Math.ceil(people / 2);
+  const src = Array.from({ length: people }, (v, i) => i + 1);
+  const selected = new Array(people);
+  const visited = Array(people).fill(false);
+
+  seatCnt = Math.ceil(people / 2);
+  
   if (seatCnt >= MAX_COL) {
-    rows = seatCnt / cols;
-    if (rows % cols > 0) ++rows;
+    cols = MAX_COL;
+    rows = Math.ceil(seatCnt / cols);
   }
   else {
     rows = 1;
     cols = seatCnt;
   }
 
+  idx = 0;
+  idx2 = 0;
+
+  console.log("people: " + people);
+  console.log("seatCnt: " + seatCnt);
+  console.log("rows: " + rows);
+  console.log("cols: " + cols);
+
+  permutation(src, people, selected, visited);
+}
+
+const makeChart = () => {
   const chart = document.getElementById('chart');
   chart.innerHTML = '';
   chart.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
-
+  
   let count = 0;
   for (let i = 0; i < rows * cols; i++) {
       const seat = document.createElement('div');
@@ -34,13 +78,49 @@ const generateChart = () => {
 
       chart.appendChild(seat);
   }
-  const arr = [];
-  // combination(arr, ) 
 }
 
-const permutation = (cnt, selected, visited) => {
+const checkPairHistory = (selected) => {
+  let person1;
+  let person2;
+
+  for (let i = 0; i < people; i+=2) {
+    person1 = selected[i];
+    if (i + 1 >= people) {
+      break;
+    }
+    person2 = selected[i + 1];
+    
+    if (pairSet[person1].has(person2) || pairSet[person2].has(person1)) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const setPairHistory = (selected) => {
+  let person1;
+  let person2;
+
+  for (let i = 0; i < people; i+=2) {
+    person1 = selected[i];
+    if (i + 1 >= people) break;
+    person2 = selected[i + 1];
+
+    pairSet[person1].add(person2);
+    pairSet[person2].add(person1);
+  }
+};
+
+const permutation = (src, cnt, selected, visited) => {
   if (cnt === 0) {
-    result.push(selected.join(' '));
+    // console.log(++idx + ")org: " + selected);
+    
+    if (!checkPairHistory(selected)) return;
+    setPairHistory(selected);
+    // makeChart();
+    console.log(++idx2 + ":: " + selected);
     return;
   }
 
@@ -48,7 +128,7 @@ const permutation = (cnt, selected, visited) => {
     if (!visited[i]) {
       visited[i] = true;
       selected[selected.length - cnt] = src[i];
-      permutation(cnt - 1, selected, visited);
+      permutation(src, cnt - 1, selected, visited);
       visited[i] = false;
     }
   }
